2e732b679529c7d7aced19cd135f3af200615d58
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-22 00:46:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,7 @@ class CashRegister
   end 
 
   def add_item(title, price, quantity=1)
-    if title.uniq? == true
-      items << title 
-    end 
+    items << title 
     total= self.total += (price * quantity)
     total
     self.the_last_transaction = price * quantity
